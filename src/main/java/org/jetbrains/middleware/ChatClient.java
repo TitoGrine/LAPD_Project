d2c616aaab7a8231db886ebd2e7163590254a8da
Andre Rocha <andrefmrocha@live.com.pt>
2021-04-14 22:25:17 +0100

    Setup interaction with json rpc internally

---
+++ src/main/java/org/jetbrains/middleware/ChatClient.java
@@ -0,0 +1,16 @@
+package org.jetbrains.middleware;
+
+import org.eclipse.lsp4j.jsonrpc.services.JsonNotification;
+import org.eclipse.lsp4j.jsonrpc.services.JsonSegment;
+
+@JsonSegment("client")
+public interface ChatClient {
+
+    /**
+     * The `client/didPostMessage` is sent by the server to all clients
+     * in a response to the `server/postMessage` notification.
+     */
+    @JsonNotification
+    void didPostMessage(ChatServer.UserMessage message);
+
+}
--- src/main/kotlin/org/jetbrains/middleware/builder/strategies/JsonRpcStrategy.kt
+++ src/main/kotlin/org/jetbrains/middleware/builder/strategies/JsonRpcStrategy.kt
@@ -9,11 +9,39 @@ import org.jetbrains.middleware.builder.RequestData
 import org.jetbrains.middleware.builder.RequestParams
 import org.jetbrains.middleware.builder.RequestResponse
 import java.lang.reflect.Type
+import java.net.Socket
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
 
 val gson = Gson()
 
-class JsonRpcStrategy(private val launcher: Launcher<*>) : APITypeStrategy<List<JsonRpcStrategy.JsonRpcObject>, CompletableFuture<*>> {
+class JsonRpcStrategy<T>(private val host: String, port: Int, interfaceClass: Class<T>, client: Any) :
+    APITypeStrategy<List<JsonRpcStrategy.JsonRpcObject>, CompletableFuture<*>> {
+
+    private val launcher: Launcher<T> by lazy {
+        // connect to the server
+        val socket = Socket(host, port)
+        // open a JSON-RPC connection for the opened socket
+        val launcher = Launcher.Builder<T>()
+            .setRemoteInterface(interfaceClass)
+            .setExecutorService(Executors.newSingleThreadExecutor())
+            .setInput(socket.getInputStream())
+            .setOutput(socket.getOutputStream())
+            .setLocalService(client)
+            .create()
+        /*
+             * Start listening for incoming messages.
+             * When the JSON-RPC connection is closed
+             * disconnect the remote client from the chat server.
+             */
+        Thread {
+            launcher.startListening().get()
+            socket.close()
+        }.start()
+
+        launcher
+    }
+
     override suspend fun sendRequest(
         host: String,
         url: String,
