af1504f4f2a05880aabccad20f9546942fb87273
TitoGrine <tito999@gmail.com>
2021-05-01 19:44:32 +0100

    fix: Removed DEBUG tracing. Added server prints.

--- src/main/kotlin/com/grpc/shared/ColorsServiceImpl.kt
+++ src/main/kotlin/com/grpc/shared/ColorsServiceImpl.kt
@@ -5,21 +5,46 @@ import com.grpc.converters.*
 import com.grpc.models.*
 
 class ColorsServiceImpl : ColorsServiceGrpcKt.ColorsServiceCoroutineImplBase() {
-    override suspend fun getRandomHexColor(request: Void): Color =
-        GenericColor_2_Color(buildHEXColor(getRandomHexColor()))
+    private fun printOperation(op: String) {
+        println()
+        println("\t\t ---- GRPC ----")
+        println(op)
+        println()
+    }
 
-    override suspend fun getRandomRGBColor(request: Void): Color =
-        GenericColor_2_Color(buildRGBColor(getRandomRGBColor()))
+    override suspend fun getRandomHexColor(request: Void): Color {
+        printOperation("getRandomHexColor()")
+        return GenericColor_2_Color(buildHEXColor(getRandomHexColor()))
+    }
 
-    override suspend fun getRandomCMYKColor(request: Void): Color =
-        GenericColor_2_Color(buildCMYKColor(getRandomCMYKColor()))
+    override suspend fun getRandomRGBColor(request: Void): Color {
+        printOperation("getRandomRGBColor()")
+        return GenericColor_2_Color(buildRGBColor(getRandomRGBColor()))
+    }
 
-    override suspend fun getRandomHSVColor(request: Void): Color =
-        GenericColor_2_Color(buildHSVColor(getRandomHSVColor()))
+    override suspend fun getRandomCMYKColor(request: Void): Color {
+        printOperation("getRandomCMYKColor()")
+        return GenericColor_2_Color(buildCMYKColor(getRandomCMYKColor()))
+    }
 
-    override suspend fun convertColor(request: ColorConversionRequest): ColorConversionResponse =
-        GenericConversionResponse_2_ConversionResponse(convertColor(ConversionRequest_2_GenericConversionRequest(request)!!))!!
+    override suspend fun getRandomHSVColor(request: Void): Color {
+        printOperation("getRandomHSVColor()")
+        return GenericColor_2_Color(buildHSVColor(getRandomHSVColor()))
+    }
 
-    override suspend fun generateColorPalette(request: ColorPaletteRequest): ColorPaletteResponse =
-        GenericPaletteResponse_2_PaletteResponse(generatePalette(PaletteRequest_2_GenericPaletteRequest(request)!!))!!
+    override suspend fun convertColor(request: ColorConversionRequest): ColorConversionResponse {
+        printOperation("convertColor()")
+        return GenericConversionResponse_2_ConversionResponse(
+            convertColor(
+                ConversionRequest_2_GenericConversionRequest(
+                    request
+                )!!
+            )
+        )!!
+    }
+
+    override suspend fun generateColorPalette(request: ColorPaletteRequest): ColorPaletteResponse {
+        printOperation("generateColorPalette()")
+        return GenericPaletteResponse_2_PaletteResponse(generatePalette(PaletteRequest_2_GenericPaletteRequest(request)!!))!!
+    }
 }
